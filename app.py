66ab235dd929552245421cdf9443a63b95bdbf71
Ketan <ketanbmahajan@gmail.com>
2024-08-13 22:17:44 -0500

    Added post model

--- app.py
+++ app.py
@@ -22,12 +22,20 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     userid = db.Column(db.String(20),unique= True, nullable=False)
     password_hash = db.Column(db.String(80), nullable=False)
-
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+class Post(db.Model):
+    '''
+    Post table for storing the post of the user
+    '''
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 # @app.route("/")
 # def hello_world():
